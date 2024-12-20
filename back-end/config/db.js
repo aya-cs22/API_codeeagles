93cd18f5565d68ab6cbd1939c98681cec8284667
aya-cs22 <aya786930@gmail.com>
2024-12-20 12:09:26 +0200

    backend

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -4,10 +4,14 @@ const dbConnection = async () => {
   try {
     console.log('Connecting to DB:', process.env.DB_URI);
     await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
     });
     console.log('Database connected successfully');
   } catch (error) {
     console.error('Database connection error:', error);
   }
 };
+
 module.exports = dbConnection;
