bb48fc79e9d21145fbbfc28e937acb791a390010
aya-cs22 <aya786930@gmail.com>
2024-12-21 22:28:17 +0200

    back

--- back-end/routes/ContactMessageRoutes.js
+++ back-end/routes/ContactMessageRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authenticate')
 const ContactMessageController = require('../controllers/ContactMessageController');
-
+// contact-us
 router.post('/contact-us', ContactMessageController.contact_us);
 router.get('/contact-us/messages', authMiddleware, ContactMessageController.get_all_messages);
 router.post('/contact-us/reply', authMiddleware, ContactMessageController.reply_to_message);
