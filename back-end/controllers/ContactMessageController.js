4472c09227d721fd3f9350629f4b26812285f2cd
aya-cs22 <aya786930@gmail.com>
2024-12-21 22:23:32 +0200

    back

--- back-end/controllers/ContactMessageController.js
+++ back-end/controllers/ContactMessageController.js
@@ -1,102 +1,102 @@
-// const ContactMessage = require("../models/ContactMessage");
-// const transporter = require('../config/mailConfig');
+const ContactMessage = require("../models/ContactMessage");
+const transporter = require('../config/mailConfig');
 
-// // contact-us
-// exports.contact_us = async (req, res) => {
-//     try {
-//         const { name, email, message } = req.body;
+// contact-us
+exports.contact_us = async (req, res) => {
+    try {
+        const { name, email, message } = req.body;
 
-//         if (!name || !email || !message) {
-//             return res.status(400).json({ message: 'All fields are required' });
-//         }
+        if (!name || !email || !message) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
 
-//         const contactMessage = new ContactMessage({
-//             name: name,
-//             email: email,
-//             message: message,
-//         });
+        const contactMessage = new ContactMessage({
+            name: name,
+            email: email,
+            message: message,
+        });
 
-//         await contactMessage.save();
+        await contactMessage.save();
 
-//         const mailOptions = {
-//             from: email,
-//             to: process.env.ADMIN_EMAIL,
-//             subject: 'New Contact Us Message',
-//             text: `You have received a new message from:
-//                    \nName: ${name}
-//                    \nEmail: ${email}
-//                    \nMessage: ${message}`,
-//         };
+        const mailOptions = {
+            from: email,
+            to: process.env.ADMIN_EMAIL,
+            subject: 'New Contact Us Message',
+            text: `You have received a new message from:
+                   \nName: ${name}
+                   \nEmail: ${email}
+                   \nMessage: ${message}`,
+        };
 
-//         const info = await transporter.sendMail(mailOptions);
+        const info = await transporter.sendMail(mailOptions);
 
-//         console.log('Email sent: ' + info.response);
+        console.log('Email sent: ' + info.response);
 
-//         return res.status(200).json({ message: 'Message sent successfully' });
+        return res.status(200).json({ message: 'Message sent successfully' });
 
-//     } catch (error) {
-//         console.error('Error:', error);
-//         return res.status(500).json({ message: 'Server error' });
-//     }
-// };
-
-
+    } catch (error) {
+        console.error('Error:', error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+};
 
 
-// exports.get_all_messages = async (req, res) => {
-//     try {
-//         if (req.user.role !== 'admin') {
-//             return res.status(403).json({ message: 'Access denied, only admins can view messages.' });
-//         }
-
-//         const messages = await ContactMessage.find();
-
-//         if (messages.length === 0) {
-//             return res.status(404).json({ message: 'No messages found' });
-//         }
-
-//         return res.status(200).json({ messages: messages });
-
-//     } catch (error) {
-//         console.error('Error:', error);
-//         return res.status(500).json({ message: 'Server error' });
-//     }
-// };
 
 
-// exports.reply_to_message = async (req, res) => {
-//     try {
-//         if (req.user.role !== 'admin') {
-//             return res.status(403).json({ message: 'Access denied, only admins can reply to messages.' });
-//         }
+exports.get_all_messages = async (req, res) => {
+    try {
+        if (req.user.role !== 'admin') {
+            return res.status(403).json({ message: 'Access denied, only admins can view messages.' });
+        }
 
-//         const { messageId, adminReply } = req.body;
+        const messages = await ContactMessage.find();
 
-//         if (!messageId || !adminReply) {
-//             return res.status(400).json({ message: 'Message ID and admin reply are required' });
-//         }
+        if (messages.length === 0) {
+            return res.status(404).json({ message: 'No messages found' });
+        }
 
-//         const message = await ContactMessage.findById(messageId);
-//         if (!message) {
-//             return res.status(404).json({ message: 'Message not found' });
-//         }
+        return res.status(200).json({ messages: messages });
 
-//         message.adminReply = adminReply;
-//         await message.save();
+    } catch (error) {
+        console.error('Error:', error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+};
 
-//         const mailOptions = {
-//             from: process.env.ADMIN_EMAIL,
-//             to: message.email,
-//             subject: 'Reply to Your Message',
-//             text: `Dear ${message.name},\n\n${adminReply}\n\nBest regards,\nAdmin`,
-//         };
 
-//         const info = await transporter.sendMail(mailOptions);
-//         console.log('Email sent: ' + info.response);
+exports.reply_to_message = async (req, res) => {
+    try {
+        if (req.user.role !== 'admin') {
+            return res.status(403).json({ message: 'Access denied, only admins can reply to messages.' });
+        }
 
-//         return res.status(200).json({ message: 'Reply sent successfully and email sent to the user', message });
-//     } catch (error) {
-//         console.error('Error:', error);
-//         return res.status(500).json({ message: 'Server error' });
-//     }
-// };
+        const { messageId, adminReply } = req.body;
+
+        if (!messageId || !adminReply) {
+            return res.status(400).json({ message: 'Message ID and admin reply are required' });
+        }
+
+        const message = await ContactMessage.findById(messageId);
+        if (!message) {
+            return res.status(404).json({ message: 'Message not found' });
+        }
+
+        message.adminReply = adminReply;
+        await message.save();
+
+        const mailOptions = {
+            from: process.env.ADMIN_EMAIL,
+            to: message.email,
+            subject: 'Reply to Your Message',
+            text: `Dear ${message.name},\n\n${adminReply}\n\nBest regards,\nAdmin`,
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Email sent: ' + info.response);
+
+        return res.status(200).json({ message: 'Reply sent successfully and email sent to the user', message });
+    } catch (error) {
+        console.error('Error:', error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+};
